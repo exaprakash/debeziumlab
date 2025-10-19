72500430f00ffab883cca074482871606324190a
exaprakash <Prakash.Subramanian@exasol.com>
2025-10-19 08:32:53 +0200

    Create kafkarun.sh

---
+++ kafkarun.sh
@@ -0,0 +1,8 @@
+cp docker-compose-kafka.yml docker-compose.yml
+export DEBEZIUM_VERSION=3.3
+SRCMYSQL="34.232.51.67"
+SINKMYSQL="44.201.54.90"
+docker compose up -d
+sleep 60
+chmod +x ./exregister_connectors.sh
+./exregister_connectors.sh ${SRCMYSQL} ${SINKMYSQL}
