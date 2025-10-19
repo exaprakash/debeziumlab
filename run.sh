715245ca20299f368f0090c9b7c8d3bcac5b9249
exaprakash <Prakash.Subramanian@exasol.com>
2025-10-19 07:50:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 
 export DEBEZIUM_VERSION=3.3
+SRCMYSQL="34.232.51.67"
+SINKMYSQL="44.201.54.90"
 docker compose up -d
 sleep 60
 chmod +x ./exregister_connectors.sh
-./exregister_connectors.sh 34.232.51.67 44.201.54.90
+./exregister_connectors.sh ${SRCMYSQL} ${SINKMYSQL}
