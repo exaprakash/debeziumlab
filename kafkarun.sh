1e37df92c10b7a820cec6b8f00beb2ae2cd4677e
exaprakash <Prakash.Subramanian@exasol.com>
2025-10-20 05:49:32 +0200

    Update kafkarun.sh

--- kafkarun.sh
+++ kafkarun.sh
@@ -1,7 +1,10 @@
+docker compose down
+rm -fr docker-compose.yml
+docker rmi -f $(docker images -a -q)
 cp docker-compose-kafka.yml docker-compose.yml
 export DEBEZIUM_VERSION=3.3
-SRCMYSQL="13.218.185.65"
-SINKMYSQL="3.239.235.43"
+SRCMYSQL="34.200.232.217"
+SINKMYSQL="44.202.186.124"
 docker compose up -d
 sleep 60
 chmod +x ./exregister_connectors.sh
