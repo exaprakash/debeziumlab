4a247df0be7c28a2e64fbb6c589f2bbbf1a58298
exaprakash <Prakash.Subramanian@exasol.com>
2025-10-21 08:38:23 +0200

    Update serverrun.sh

--- debezium-server/serverrun.sh
+++ debezium-server/serverrun.sh
@@ -2,5 +2,5 @@ docker stop debezium-server-exasol
 docker rmi -f $(docker images -a -q)
 docker run -d \
   --name debezium-server-exasol \
-  -v ./application.yaml:/debezium/conf/application.yaml \
+  -v ./application.yml:/debezium/conf/application.yaml \
   quay.io/debezium/server:3.3
